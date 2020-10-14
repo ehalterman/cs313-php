a888a69c3ef72cb69306cba7cb61663c7433f916
ehalterman <hal17034@byui.edu>
2020-10-13 19:39:43 -0600

    team week 5 first try

---
+++ web/team/week5/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Week 5</title>
+</head>
+<body>
+    <h1>Scripture Resources</h1>
+    <?php
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    foreach ($db->query('SELECT * FROM Scriptures') as $row)
+    {
+        echo '<p><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' -</b> "' . $row['content'] . '"<br/>';
+    }
+    ?>
+    
+
+    
+</body>
+</html>
