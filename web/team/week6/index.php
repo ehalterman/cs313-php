aefdb82746d5fbdb35b2ccf541d59612aaafe709
ehalterman <hal17034@byui.edu>
2020-10-20 20:19:06 -0600

    update

--- web/team/week6/index.php
+++ web/team/week6/index.php
@@ -21,22 +21,29 @@ try {
 $topics = $db->query('SELECT * FROM topic');
 
 $book = filter_input(INPUT_POST, 'book', FILTER_SANITIZE_STRING);
-if (!empty ($book)){
-$chapter = filter_input(INPUT_POST, 'chapter', FILTER_SANITIZE_NUMBER_INT);
-$verse = filter_input(INPUT_POST, 'verse', FILTER_SANITIZE_NUMBER_INT);
-$content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRING);
-$topicInput = filter_input(INPUT_POST, 'topic[]', FILTER_SANITIZE_STRING);
+if (!empty($book)) {
+    $chapter = filter_input(INPUT_POST, 'chapter', FILTER_SANITIZE_NUMBER_INT);
+    $verse = filter_input(INPUT_POST, 'verse', FILTER_SANITIZE_NUMBER_INT);
+    $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRING);
+    $topicInput = filter_input(INPUT_POST, 'topic[]', FILTER_SANITIZE_STRING);
 
 
-$stmt = $db->prepare('INSERT INTO Scriptures (id, book, chapter, verse, content) VALUES (default, :book, :chapter, :verse, :content)');
-$stmt->bindValue(':book', $book, PDO::PARAM_STR);
-$stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-$stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-$stmt->bindValue(':content', $content, PDO::PARAM_STR);
-$stmt->execute();
+    $stmt = $db->prepare('INSERT INTO Scriptures (id, book, chapter, verse, content) VALUES (default, :book, :chapter, :verse, :content)');
+    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+    $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+    $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+    $stmt->execute();
+
+$scriptureId = $db->lastInsertId("scriptures_id_seq");
+
+foreach($topicInput as $topic){
+    $stmt = $db->prepare('INSERT INTO links (id, topicid, scriptureid) VALUES (default, :topicid, :scriptureid)');
+    $stmt->bindValue(':topicid', $topic, PDO::PARAM_INT);
+    $stmt->bindValue(':scriptureid', $scriptureId, PDO::PARAM_INT);
+    $stmt->execute();
 }
-//$scriptureId = $db->lastInsertId("scripture_id_seq");
-
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,7 +53,8 @@ $stmt->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        label, input {
+        label,
+        input {
             margin: 10px;
         }
     </style>
