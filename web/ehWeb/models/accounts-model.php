af242f718f25c9b240f30489f0612b776310e88d
ehalterman <hal17034@byui.edu>
2020-10-27 16:49:33 -0600

    update

--- web/ehWeb/models/accounts-model.php
+++ web/ehWeb/models/accounts-model.php
@@ -1,6 +1,29 @@
 <?php
 
-require_once 'connection.php';
+function phpConnection(){
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }
+    catch (PDOException $ex)
+    {
+      echo ("failed");
+      die();
+    }
+  }
 
 function getUser($username){
     $db = phpConnection();
